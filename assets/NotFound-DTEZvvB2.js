39a10b501cefb42f2279a51a90e9b91e0349520f
BGal23 <bartek.gal23@gmail.com>
2024-03-19 18:56:41 +0100

    Updates

--- assets/NotFound-DTEZvvB2.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-0WjipS2X.js";const r=()=>t.jsx(t.Fragment,{children:"NotFound"});export{r as default};
--- assets/Register-CZSmlAzY.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-0WjipS2X.js";const t=()=>e.jsx(e.Fragment,{children:"Register"});export{t as default};
