d469f423edf6c0be5a0befbf9ef0ee46bc1b5773
BGal23 <bartek.gal23@gmail.com>
2024-03-23 22:04:27 +0100

    Updates

--- assets/NotFound-BVP9ACWi.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DOQKCt9C.js";const r=()=>t.jsx(t.Fragment,{children:"NotFound"});export{r as default};
---
+++ assets/NotFound-c2nI4eeH.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-A9Q64IW7.js";const r=()=>o.jsx("h1",{children:"Not Found"});export{r as default};
